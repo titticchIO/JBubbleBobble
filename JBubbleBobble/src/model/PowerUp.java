4b76280dd568efd483421fedef15c3daa997f1d1
Flavio Mattoni <titticchio@gmail.com>
2024-07-07 12:12:00 +0200

    First addings to PowerUp
    Ho aggiunto i punti che da il powerup, la durata del powerup, un
    booleano se il powerup è temporaneo o no.

--- JBubbleBobble/src/model/PowerUp.java
+++ JBubbleBobble/src/model/PowerUp.java
@@ -1,5 +1,62 @@
 package model;
 
-public abstract class PowerUp extends Entity{
-
+public abstract class PowerUp extends Entity{	
+	
+	/*
+	 * punti dati dal powerUp
+	 */
+	private int point;
+	
+	/*
+	 * durata dell'effetto del powerup
+	 */
+	private float duration;
+	
+	/*
+	 * indica se il powerup è temporaneo
+	 */	
+	private boolean temporariness;
+	
+	
+	public PowerUp(int point, float duration, boolean temporariness) {
+		this.point = point;
+		this.duration = duration;
+		this.temporariness = temporariness;
+	}
+	
+	
+	/*
+	 * getters e setters
+	 */
+	public int getPoint() {
+		return point;
+	}
+	
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	public float getDuration() {
+		return duration;
+	}
+	
+	public void setDuration(float duration) {
+		this.duration = duration;
+	}
+	
+	public boolean getTemporariness() {
+		return temporariness;
+	}
+	
+	public void setTemporariness(boolean temporariness) {
+		this.temporariness = temporariness;
+	}
+	
+	/*
+	 * quando il powerup viene attivato
+	 */
+	public void activate() {
+		
+	}
+	
 }
