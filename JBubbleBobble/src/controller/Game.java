5bd9361d840934c0c5e35935cf104d00a23f19b5
Massimo Michele Morgantini <massimo.m.morgantini@gmail.com>
2024-07-10 16:21:36 +0200

    Modify Game (game loop).
    Aggiornamento non definitivo con problemi da risolvere.

--- JBubbleBobble/src/controller/Game.java
+++ JBubbleBobble/src/controller/Game.java
@@ -8,6 +8,7 @@ public class Game implements Runnable {
 	private Player player;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
+	private final int UPS_SET = 200;
 	
 	
 	
@@ -28,27 +29,50 @@ public class Game implements Runnable {
 	@Override
 	public void run() {
 		double timePerFrame = 1000000000.0 / FPS_SET;
-		long lastFrame = System.nanoTime();
-		long now = System.nanoTime();
+		double timePerUpdate = 1000000000.0 / UPS_SET;
+		
+		long previousTime = System.nanoTime();
 
 		int frames = 0;
+		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
+		
+		double deltaU = 0;
+		double deltaF = 0;
 
 		while (true) {
-
-			now = System.nanoTime();
-			if (now - lastFrame >= timePerFrame) {
-				lastFrame = now;
-				frames++;
-				player.updateEntity();
+			long currentTime = System.nanoTime();
+			
+			deltaU += (currentTime - previousTime) / timePerUpdate;
+			deltaF += (currentTime - previousTime) / timePerFrame;
+			previousTime = currentTime;
+			
+			if (deltaU >= 1) {
+//				update();
+				updates++;
+				deltaU--;
+			}
+			
+			if (deltaF >= 1) {
 				gameFrame.repaint();
+				deltaF--;
+				deltaU--;
+				
 			}
-
-			if (System.currentTimeMillis() - lastCheck >= 1000) {
-				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames);
-				frames = 0;
-			}
+			
+//			if (now - lastFrame >= timePerFrame) {
+//				player.updateEntity();
+//				gameFrame.repaint();
+//				lastFrame = now;
+//				frames++;
+//			}
+//
+//			if (System.currentTimeMillis() - lastCheck >= 1000) {
+//				lastCheck = System.currentTimeMillis();
+//				System.out.println("FPS: " + frames + " | UPS: " + updates);
+//				frames = 0;
+//				updates = 0;
+//			}
 		}
 	}
 	
