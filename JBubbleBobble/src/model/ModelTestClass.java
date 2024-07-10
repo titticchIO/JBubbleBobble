250f0813b0a9e1a921a14cdeb734eade61847346
DaniZu <zu.dani2004@gmail.com>
2024-07-10 16:58:43 +0200

    Add LevelLoader
    Da completare

--- JBubbleBobble/src/model/ModelTestClass.java
+++ JBubbleBobble/src/model/ModelTestClass.java
@@ -1,11 +1,10 @@
 package model;
 
-import model.entity.Hitbox;
+
 
 public class ModelTestClass {
 	public static void main(String[] args) {
-		Hitbox h1=new Hitbox(0, 0, 10, 20);
-		Hitbox h2=new Hitbox(9, 10, 10, 10);
-		System.out.println(h1.hit(h2));
+		String path="Livello1";
+		System.out.println(LevelLoader.loadLevel(path));
 	}
 }
