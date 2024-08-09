da6248b518d98a155c85803787f48416771aa138
Massimo Michele <massimo.m.morgantini@gmail.com>
2024-08-09 15:16:53 +0200

    Iniziato a implementare il card layout

--- JBubbleBobble/src/game/view/GameFrame.java
+++ JBubbleBobble/src/game/view/GameFrame.java
@@ -1,9 +1,10 @@
 package game.view;
 
-
 import java.awt.event.ActionListener;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,49 +14,45 @@ import game.controller.Game;
 import game.controller.PlayerController;
 
 public class GameFrame extends JFrame {
-    private GamePanel gamePanel;
-    private JLabel scoreLabel;
-    private JLabel topScoreLabel;
-
-
-
+	
+	public enum Screen {
+		MENU,
+		GAME
+	}
+	
+	
+	private GamePanel gamePanel;
+	private MenuPanel menuPanel;
 
 	public GameFrame(Game game, PlayerController playerController, ActionListener actionListener) {
-		this.gamePanel = new GamePanel(actionListener);
+		setLayout(new CardLayout());
+		
+		
+		gamePanel = new GamePanel();
+		menuPanel = new MenuPanel(actionListener);
 		// Attach the PlayerController as a KeyListener
 		addKeyListener(playerController);
 		setFocusable(true); // Ensure the frame is focusable
 		setFocusTraversalKeysEnabled(false); // Disable focus traversal keys
 
-        // Creare un pannello superiore per mostrare il punteggio
-        JPanel topPanel = new JPanel();
-        scoreLabel = new JLabel("Score: 0");
-        topScoreLabel = new JLabel("Top Score: 100");
-        topPanel.add(scoreLabel);
-        topPanel.add(topScoreLabel);
+		add(gamePanel, Screen.GAME);
+		add(menuPanel, Screen.MENU);
 
-        // Imposta il layout e aggiungi il pannello superiore
-        setLayout(new BorderLayout());
-        add(topPanel, BorderLayout.NORTH);
-        add(gamePanel, BorderLayout.CENTER);
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 
-        
+	public GamePanel getGamePanel() {
+		return gamePanel;
+	}
 
-        setResizable(false);
-        pack();
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-
-    public GamePanel getGamePanel() {
-        return gamePanel;
-    }
-    
-    @Override
-    public void repaint() {
-        super.repaint();
-        gamePanel.repaint();
-    }
+	@Override
+	public void repaint() {
+		super.repaint();
+		gamePanel.repaint();
+	}
 
 }
--- JBubbleBobble/src/game/view/GamePanel.java
+++ JBubbleBobble/src/game/view/GamePanel.java
@@ -1,5 +1,6 @@
 package game.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -8,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import game.controller.gamestates.GameState;
@@ -19,18 +21,31 @@ public class GamePanel extends JPanel {
 	private LevelView levelView;
 	private MovingEntityView playerView;
 	private BufferedImage tilesImage;
-	private MenuPanel menuPanel;
+	private JLabel scoreLabel;
+	private JLabel topScoreLabel;
 
 //	private MenuPanel menuPanel
 
 //	NON AGGIUNGERE IL PATTERN SINGLETON!!!!!
 
-	public GamePanel(ActionListener actionListener) {
+	public GamePanel() {
 		setPanelSize();
 
-		menuPanel = new MenuPanel(actionListener);
-		add(menuPanel);
+		JPanel game = new JPanel();
+		game.setPreferredSize(new Dimension((int) (Level.GAME_WIDTH * SCALE), (int) (Level.GAME_HEIGHT * SCALE)));
+		// Creare un pannello superiore per mostrare il punteggio
+		JPanel score = new JPanel();
+		scoreLabel = new JLabel("Score: 0");
+		topScoreLabel = new JLabel("Top Score: 100");
+		score.add(scoreLabel);
+		score.add(topScoreLabel);
 
+		// Imposta il layout e aggiungi il pannello superiore
+		setLayout(new BorderLayout());
+		add(score, BorderLayout.NORTH);
+		add(game, BorderLayout.CENTER);
+		
+		
 	}
 
 	private void initPlayingClasses(LevelView levelView) {
@@ -49,7 +64,7 @@ public class GamePanel extends JPanel {
 	}
 
 	private void setPanelSize() {
-		Dimension size = new Dimension((int) (Level.GAME_WIDTH*SCALE), (int) (Level.GAME_HEIGHT*SCALE));
+		Dimension size = new Dimension((int) (Level.GAME_WIDTH * SCALE), (int) (Level.GAME_HEIGHT * SCALE)+50);
 		setPreferredSize(size);
 	}
 
@@ -62,57 +77,42 @@ public class GamePanel extends JPanel {
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		
+
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.translate(SCALE, SCALE);
 		g2d.scale(SCALE, SCALE);
 
-		switch (GameState.state) {
-		case MENU:
+		super.paintComponent(g2d);
+		// Usa double buffering per disegnare su un'immagine temporanea prima di
+		// dipingerla sul JPanel
+		Image doubleBufferedImage = createImage(getWidth(), getHeight());
+		Graphics doubleBufferedGraphics = doubleBufferedImage.getGraphics();
 
-			menuPanel.setVisible(true);
-
-			break;
-		case PLAYING:
-
-			menuPanel.setVisible(false);
-
-			super.paintComponent(g2d);
-			// Usa double buffering per disegnare su un'immagine temporanea prima di
-			// dipingerla sul JPanel
-			Image doubleBufferedImage = createImage(getWidth(), getHeight());
-			Graphics doubleBufferedGraphics = doubleBufferedImage.getGraphics();
-
-			if (tilesImage != null) {
-				doubleBufferedGraphics.drawImage(tilesImage, 0, 0, this);
-			}
-			if (playerView != null) {
-				playerView.render(doubleBufferedGraphics);
-			}
-			if (levelView.getEnemies() != null) {
-				for (MovingEntityView e : levelView.getEnemies()) {
-					if (!e.isToDelete())
-						e.render(doubleBufferedGraphics);
-					else
-						e.delete(doubleBufferedGraphics);
-				}
-			}
-			if (levelView.getBubbles() != null) {
-				for (MovingEntityView b : levelView.getBubbles()) {
-					if (!b.isToDelete())
-						b.render(doubleBufferedGraphics);
-					else
-						b.delete(doubleBufferedGraphics);
-				}
-			}
-
-			g2d.drawImage(doubleBufferedImage, 0, 0, this);
-			doubleBufferedGraphics.dispose();
-			break;
-		default:
-			break;
-
+		if (tilesImage != null) {
+			doubleBufferedGraphics.drawImage(tilesImage, 0, 0, this);
 		}
+		if (playerView != null) {
+			playerView.render(doubleBufferedGraphics);
+		}
+		if (levelView.getEnemies() != null) {
+			for (MovingEntityView e : levelView.getEnemies()) {
+				if (!e.isToDelete())
+					e.render(doubleBufferedGraphics);
+				else
+					e.delete(doubleBufferedGraphics);
+			}
+		}
+		if (levelView.getBubbles() != null) {
+			for (MovingEntityView b : levelView.getBubbles()) {
+				if (!b.isToDelete())
+					b.render(doubleBufferedGraphics);
+				else
+					b.delete(doubleBufferedGraphics);
+			}
+		}
+
+		g2d.drawImage(doubleBufferedImage, 0, 0, this);
+		doubleBufferedGraphics.dispose();
 
 	}
 
