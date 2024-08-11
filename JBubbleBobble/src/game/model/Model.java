baa4e246e6cb336fa6b93e47539e935e29554cad
Daniele-Zu <zu.dani2004@gmail.com>
2024-08-11 11:24:53 +0200

    New class Model, View

---
+++ JBubbleBobble/src/game/model/Model.java
@@ -0,0 +1,57 @@
+package game.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Observable;
+
+import game.model.level.Level;
+
+public class Model extends Observable {
+	private List<Level> levels;
+	private Iterator<Level> levelIterator;
+	private Level currentLevel;
+
+	private long score;
+	private long highScore;
+
+	private static Model instance;
+
+	public static Model getInstance() {
+		if (instance == null)
+			instance = new Model();
+		return instance;
+	}
+
+	private Model() {
+		levels = new ArrayList<Level>();
+		addLevels();
+		currentLevel = levels.getFirst();
+		levelIterator = levels.iterator();
+	}
+
+	public Level getCurrentLevel() {
+		return currentLevel;
+	}
+
+	public void addLevels() {
+		levels.add(new Level(111));
+		levels.add(new Level(2));
+		levels.add(new Level(3));
+		levels.add(new Level(333));
+		levels.add(new Level(4));
+		levels.add(new Level(5));
+		levels.add(new Level(777));
+	}
+
+	public void nextLevel() {
+		currentLevel = levelIterator.next();
+	}
+
+	public void updateModel() {
+		currentLevel.updateLevel();
+		setChanged();
+		notifyObservers();
+	}
+
+}
