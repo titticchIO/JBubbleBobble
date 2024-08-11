6ab9141ae30e4610204243c2d5ea3a1dd791154a
Daniele-Zu <zu.dani2004@gmail.com>
2024-08-11 11:25:05 +0200

    Minor changes

--- JBubbleBobble/src/game/view/View.java
+++ JBubbleBobble/src/game/view/View.java
@@ -1,5 +1,16 @@
 package game.view;
 
 public class View {
+	private static View instance;
+
+	public static View getInstance() {
+		if (instance == null)
+			instance = new View();
+		return instance;
+	}
+
+	private View() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
