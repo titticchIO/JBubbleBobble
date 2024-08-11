cd191468c94234b2d62e18c1038ebea6803d2251
Massimo Michele <massimo.m.morgantini@gmail.com>
2024-08-11 17:10:20 +0200

    Modify LevelReader with static method

--- JBubbleBobble/src/editor/model/LevelReader.java
+++ JBubbleBobble/src/editor/model/LevelReader.java
@@ -6,23 +6,16 @@ import java.util.*;
 public class LevelReader {
 
 	public static final String LEVELS_REL_PATH = "resources/levels";
-	private List<String> levels;
 
-	public LevelReader() {
-		levels = new ArrayList<String>();
-		
+	public static List<String> getLevels() {
+		List<String> levels = new ArrayList<String>();
+
 		File dir = new File(LEVELS_REL_PATH);
-		
+
 		for (File file : dir.listFiles()) {
 			levels.add(file.getName().replaceAll("[^0-9]", ""));
 		}
-	}
-	
-	public List<String> getLevels() {
+		
 		return levels;
-	}
-	
-	public void addFileNumber(String FileNumber) {
-		levels.add(FileNumber);
 	}
 }
