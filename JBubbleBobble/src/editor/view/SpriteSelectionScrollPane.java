ae8e81e8a94efe731bfdb69e5921975047d96291
Massimo Michele <massimo.m.morgantini@gmail.com>
2024-08-12 17:07:17 +0200

    Add PinkCandy image to SpriteSelectionScrollPane

--- JBubbleBobble/src/editor/view/SpriteSelectionScrollPane.java
+++ JBubbleBobble/src/editor/view/SpriteSelectionScrollPane.java
@@ -36,6 +36,8 @@ public class SpriteSelectionScrollPane extends JScrollPane {
         addPlayers();
         // Aggiunta di un bottone vuoto
         others.add(new SelectionButton(ImageLoader.importImg("/EmptyTile.png"), " "));
+        others.add(new SelectionButton(ImageLoader.importImg("/powerups/PinkCandy.png"), " "));
+        
 
         setSize();
         setLayout(new ScrollPaneLayout());
