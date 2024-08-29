0111f145b9fc61eb879f32b3f0fc505089138a35
Daniele-Zu <zu.dani2004@gmail.com>
2024-08-29 18:18:33 +0200

    Update MenuScreen, Fix EndPanel

--- JBubbleBobble/src/game/view/EndPanel.java
+++ JBubbleBobble/src/game/view/EndPanel.java
@@ -2,6 +2,7 @@ package game.view;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -14,12 +15,12 @@ public class EndPanel extends JPanel {
 		WIN, LOSS
 	}
 
-	private BufferedImage img;
+	private Image img;
 
 	public EndPanel(Ending ending) {
 		img = switch (ending) {
 		case WIN -> ImageLoader.importImg("/menu/WinScreen.png");
-		case LOSS -> ImageLoader.importImg("/menu/LooseScreen.png");
+		case LOSS -> AnimationLoader.loadEntityImage("/menu/LooseScreen.gif");
 		};
 		setSize(new Dimension((int) (Level.GAME_WIDTH * LevelPanel.SCALE),
 				(int) (Level.GAME_HEIGHT * LevelPanel.SCALE)));
