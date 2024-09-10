3fea1a679b23bb506675d49c3a4fbae737330d8d
Massimo Michele <massimo.m.morgantini@gmail.com>
2024-09-10 15:59:13 +0200

    Upated powerup nubero of levels skipped

--- JBubbleBobble/src/game/model/powerups/OrangeParasol.java
+++ JBubbleBobble/src/game/model/powerups/OrangeParasol.java
@@ -1,13 +1,13 @@
 package game.model.powerups;
 
 /**
- * The {@code OrangeParasol} item allows the player to skip 1 level.
+ * The {@code OrangeParasol} item allows the player to skip 2 level.
  */
 public class OrangeParasol extends Parasol {
 
 	public static final int SPAWN_CONDITION = 2;
 
 	public OrangeParasol() {
-		super(0, 0, 1);
+		super(0, 0, 2);
 	}
 }
