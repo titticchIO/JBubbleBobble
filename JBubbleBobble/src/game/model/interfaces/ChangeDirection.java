07a295402795eb2d11e071835285a0b2d582fa8d
Daniele-Zu <zu.dani2004@gmail.com>
2024-09-10 15:02:49 +0200

    New Javadoc

--- JBubbleBobble/src/game/model/interfaces/ChangeDirection.java
+++ JBubbleBobble/src/game/model/interfaces/ChangeDirection.java
@@ -1,7 +1,8 @@
 package game.model.interfaces;
 
 public interface ChangeDirection {
-
+	/**
+	 * Handles the bouncing behavior of the enemies when hitting tiles.
+	 */
 	void changeDirection();
-
 }
--- JBubbleBobble/src/game/model/interfaces/Shooting.java
+++ JBubbleBobble/src/game/model/interfaces/Shooting.java
@@ -1,5 +1,8 @@
 package game.model.interfaces;
 
 public interface Shooting {
+	/**
+	 * Handles the action of shooting a projectile
+	 */
 	void shoot();
 }
