f7fc99d1f71403362885ec158b7bc893e7e2db68
Daniele-Zu <zu.dani2004@gmail.com>
2024-09-11 11:08:05 +0200

    Spostati 2 ActionListener in editor.ActionListenerManager

--- JBubbleBobble/src/editor/controller/ActionListenersManager.java
+++ JBubbleBobble/src/editor/controller/ActionListenersManager.java
@@ -1,6 +1,8 @@
 package editor.controller;
 
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 
@@ -9,112 +11,174 @@ import editor.view.EditorFrame;
 import editor.view.EditorPanel;
 import editor.view.LevelsPopUpMenu;
 import editor.view.SelectionButton;
+import editor.view.Sprite;
 import editor.view.LevelsPopUpMenu.MenuType;
 import editor.view.SpriteSelectionScrollPane;
+import static editor.view.EditorPanel.SQUARE_SIZE;
 
 /**
  * Manages the creation of action listeners for various UI components.
  */
 public class ActionListenersManager {
 
-    /**
-     * Creates an ActionListener to create a new grid.
-     * 
-     * @param editorPanel     the panel where the grid is displayed
-     * @param selectionPane   the pane with sprite selection
-     * @return the ActionListener for creating a new grid
-     */
-    public static ActionListener newGridButton(EditorPanel editorPanel, SpriteSelectionScrollPane selectionPane) {
-        return e -> {
-            EditorFrame ef = EditorFrame.getInstance();
-            // Action to create a new grid
-            LevelManager.emptyLevel(); // Assuming this empties the current level
-            ef.getEditorPanel().reset();
-            ef.setActualLevelNumber("");
-            ef.getActualLevel().setText("Level " + ef.getActualLevelNumber());
-            ef.getEditorPanel().repaint(); // Repaint the frame
-            ef.revalidate(); // Refresh the layout of the frame
-            ef.repaint(); // Repaint the frame
-        };
-    }
+	/**
+	 * Creates an ActionListener to create a new grid.
+	 * 
+	 * @param editorPanel   the panel where the grid is displayed
+	 * @param selectionPane the pane with sprite selection
+	 * @return the ActionListener for creating a new grid
+	 */
+	public static ActionListener newGridButton(EditorPanel editorPanel, SpriteSelectionScrollPane selectionPane) {
+		return e -> {
+			EditorFrame ef = EditorFrame.getInstance();
+			// Action to create a new grid
+			LevelManager.emptyLevel(); // Assuming this empties the current level
+			ef.getEditorPanel().reset();
+			ef.setActualLevelNumber("");
+			ef.getActualLevel().setText("Level " + ef.getActualLevelNumber());
+			ef.getEditorPanel().repaint(); // Repaint the frame
+			ef.revalidate(); // Refresh the layout of the frame
+			ef.repaint(); // Repaint the frame
+		};
+	}
 
-    /**
-     * Creates an ActionListener to open a grid.
-     * 
-     * @param levelSelectionPopup the popup menu for level selection
-     * @param openGridButton      the button to trigger the popup
-     * @return the ActionListener for opening a grid
-     */
-    public static ActionListener openGridButton(LevelsPopUpMenu levelSelectionPopup, JButton openGridButton) {
-        return e -> levelSelectionPopup.show(openGridButton, 0, openGridButton.getHeight());
-    }
+	/**
+	 * Creates an ActionListener to open a grid.
+	 * 
+	 * @param levelSelectionPopup the popup menu for level selection
+	 * @param openGridButton      the button to trigger the popup
+	 * @return the ActionListener for opening a grid
+	 */
+	public static ActionListener openGridButton(LevelsPopUpMenu levelSelectionPopup, JButton openGridButton) {
+		return e -> levelSelectionPopup.show(openGridButton, 0, openGridButton.getHeight());
+	}
 
-    /**
-     * Creates an ActionListener to save the current level.
-     * 
-     * @param saveLevelPopup  the popup menu for saving the level
-     * @param saveLevelButton the button to trigger the popup
-     * @return the ActionListener for saving the level
-     */
-    public static ActionListener saveLevelButton(LevelsPopUpMenu saveLevelPopup, JButton saveLevelButton) {
-        return e -> saveLevelPopup.show(saveLevelButton, 0, saveLevelButton.getHeight());
-    }
+	/**
+	 * Creates an ActionListener to save the current level.
+	 * 
+	 * @param saveLevelPopup  the popup menu for saving the level
+	 * @param saveLevelButton the button to trigger the popup
+	 * @return the ActionListener for saving the level
+	 */
+	public static ActionListener saveLevelButton(LevelsPopUpMenu saveLevelPopup, JButton saveLevelButton) {
+		return e -> saveLevelPopup.show(saveLevelButton, 0, saveLevelButton.getHeight());
+	}
 
-    /**
-     * Creates an ActionListener to delete a level.
-     * 
-     * @param deleteLevelPopup  the popup menu for deleting the level
-     * @param deleteLevelButton the button to trigger the popup
-     * @return the ActionListener for deleting the level
-     */
-    public static ActionListener deleteLevelButton(LevelsPopUpMenu deleteLevelPopup, JButton deleteLevelButton) {
-        return e -> deleteLevelPopup.show(deleteLevelButton, 0, deleteLevelButton.getHeight());
-    }
+	/**
+	 * Creates an ActionListener to delete a level.
+	 * 
+	 * @param deleteLevelPopup  the popup menu for deleting the level
+	 * @param deleteLevelButton the button to trigger the popup
+	 * @return the ActionListener for deleting the level
+	 */
+	public static ActionListener deleteLevelButton(LevelsPopUpMenu deleteLevelPopup, JButton deleteLevelButton) {
+		return e -> deleteLevelPopup.show(deleteLevelButton, 0, deleteLevelButton.getHeight());
+	}
 
-    /**
-     * Creates an ActionListener to handle different level operations like open, save, or delete.
-     * 
-     * @param levelsPopUpMenu the popup menu for level operations
-     * @param level           the level to operate on
-     * @param menuType        the type of operation (OPEN, SAVE, DELETE)
-     * @param editorFrame     the frame that displays the editor
-     * @return the ActionListener for handling the level operation
-     */
-    public static ActionListener levelHandlers(LevelsPopUpMenu levelsPopUpMenu, String level, MenuType menuType, EditorFrame editorFrame) {
-        return e -> {
-            int numero = Integer.parseInt(level);
-            switch (menuType) {
-                case OPEN -> levelsPopUpMenu.handleOpenLevel(numero, editorFrame);
-                case SAVE -> levelsPopUpMenu.handleSaveLevel(numero, editorFrame);
-                case DELETE -> levelsPopUpMenu.handleDeleteLevel(numero, editorFrame);
-            }
-        };
-    }
+	/**
+	 * Creates an ActionListener to handle different level operations like open,
+	 * save, or delete.
+	 * 
+	 * @param levelsPopUpMenu the popup menu for level operations
+	 * @param level           the level to operate on
+	 * @param menuType        the type of operation (OPEN, SAVE, DELETE)
+	 * @param editorFrame     the frame that displays the editor
+	 * @return the ActionListener for handling the level operation
+	 */
+	public static ActionListener levelHandlers(LevelsPopUpMenu levelsPopUpMenu, String level, MenuType menuType,
+			EditorFrame editorFrame) {
+		return e -> {
+			int numero = Integer.parseInt(level);
+			switch (menuType) {
+			case OPEN -> levelsPopUpMenu.handleOpenLevel(numero, editorFrame);
+			case SAVE -> levelsPopUpMenu.handleSaveLevel(numero, editorFrame);
+			case DELETE -> levelsPopUpMenu.handleDeleteLevel(numero, editorFrame);
+			}
+		};
+	}
 
-    /**
-     * Creates an ActionListener to handle saving a level with a specific number.
-     * 
-     * @param lvlPopMen       the popup menu for level operations
-     * @param newLevelNumber  the new level number to save
-     * @param editorFrame     the frame that displays the editor
-     * @return the ActionListener for handling level saving
-     */
-    public static ActionListener handleSaveLevel(LevelsPopUpMenu lvlPopMen, int newLevelNumber, EditorFrame editorFrame) {
-        return e -> lvlPopMen.handleSaveLevel(newLevelNumber, editorFrame);
-    }
+	/**
+	 * Creates an ActionListener to handle saving a level with a specific number.
+	 * 
+	 * @param lvlPopMen      the popup menu for level operations
+	 * @param newLevelNumber the new level number to save
+	 * @param editorFrame    the frame that displays the editor
+	 * @return the ActionListener for handling level saving
+	 */
+	public static ActionListener handleSaveLevel(LevelsPopUpMenu lvlPopMen, int newLevelNumber,
+			EditorFrame editorFrame) {
+		return e -> lvlPopMen.handleSaveLevel(newLevelNumber, editorFrame);
+	}
 
-    /**
-     * Creates an ActionListener to handle selection of a button in the sprite selection pane.
-     * 
-     * @param spriteSelectionScrollPane the pane that contains sprite selection buttons
-     * @param b                         the button to select
-     * @return the ActionListener for handling button selection
-     */
-    public static ActionListener addSelectionButton(SpriteSelectionScrollPane spriteSelectionScrollPane, SelectionButton b) {
-        return e -> {
-            spriteSelectionScrollPane.deselectAllButtons();
-            b.setSelected(true);
-            spriteSelectionScrollPane.setCurrentButton(b);
-        };
-    }
+	/**
+	 * Creates an ActionListener to handle selection of a button in the sprite
+	 * selection pane.
+	 * 
+	 * @param spriteSelectionScrollPane the pane that contains sprite selection
+	 *                                  buttons
+	 * @param b                         the button to select
+	 * @return the ActionListener for handling button selection
+	 */
+	public static ActionListener addSelectionButton(SpriteSelectionScrollPane spriteSelectionScrollPane,
+			SelectionButton b) {
+		return e -> {
+			spriteSelectionScrollPane.deselectAllButtons();
+			b.setSelected(true);
+			spriteSelectionScrollPane.setCurrentButton(b);
+		};
+	}
+
+	// Editor Panel Action Listeners
+
+	/**
+	 * Updates the Sprite at the specified coordinates to the current Sprite
+	 * selected
+	 * 
+	 * @param editorPanel the JPanel containing the Sprite
+	 * @param sprite      the Sprite to be updated
+	 * @param x           the x coordinate of the Sprite
+	 * @param y           the y coordinate of the Sprite
+	 * @return the ActionListener to perform the Action
+	 */
+	public static MouseAdapter updateSpriteClick(EditorPanel editorPanel, int x, int y) {
+		return new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				editorPanel.setMousePressed(true);
+				editorPanel.updateSprite(x, y);
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				editorPanel.setMousePressed(false);
+			}
+		};
+	}
+
+	/**
+	 * Updates the Sprite at the specified coordinates to the current Sprite
+	 * selected
+	 * 
+	 * @param editorPanel the JPanel containing the Sprite
+	 * @param sprite      the Sprite to be updated
+	 * @param x           the x coordinate of the Sprite
+	 * @param y           the y coordinate of the Sprite
+	 * @return the ActionListener to perform the Action
+	 */
+	public static MouseAdapter updateSpriteDrag(EditorPanel editorPanel, int x, int y) {
+		return new MouseAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if (editorPanel.isMousePressed()) {
+					int x = (e.getXOnScreen() - editorPanel.getLocationOnScreen().x) / SQUARE_SIZE;
+					int y = (e.getYOnScreen() - editorPanel.getLocationOnScreen().y) / SQUARE_SIZE;
+					try {
+						editorPanel.updateSprite(x, y);
+					} catch (Exception ignored) {
+					}
+				}
+			}
+		};
+	}
+
 }
--- JBubbleBobble/src/editor/view/EditorPanel.java
+++ JBubbleBobble/src/editor/view/EditorPanel.java
@@ -10,160 +10,152 @@ import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 
 import editor.model.LevelManager;
+import static editor.controller.ActionListenersManager.updateSpriteClick;
+import static editor.controller.ActionListenersManager.updateSpriteDrag;
 import game.view.AnimationAndImagesLoader;
 
 /**
- * EditorPanel is responsible for displaying and interacting with the game editor grid.
- * It handles mouse events to update the grid with selected sprites.
+ * EditorPanel is responsible for displaying and interacting with the game
+ * editor grid. It handles mouse events to update the grid with selected
+ * sprites.
  */
 public class EditorPanel extends JPanel {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    // Public static constants
-    public static final int ROWS = 24;
-    public static final int COLS = 30;
-    public static final int DEFAULT_SQUARE_SIZE = 20;
-    public static final float SCALE = 1;
-    public static final int SQUARE_SIZE = (int) (DEFAULT_SQUARE_SIZE * SCALE);
-    public static final int PANEL_WIDTH = SQUARE_SIZE * COLS;
-    public static final int PANEL_HEIGHT = SQUARE_SIZE * ROWS;
+	// Public static constants
+	public static final int ROWS = 24;
+	public static final int COLS = 30;
+	public static final int DEFAULT_SQUARE_SIZE = 20;
+	public static final float SCALE = 1;
+	public static final int SQUARE_SIZE = (int) (DEFAULT_SQUARE_SIZE * SCALE);
+	public static final int PANEL_WIDTH = SQUARE_SIZE * COLS;
+	public static final int PANEL_HEIGHT = SQUARE_SIZE * ROWS;
 
-    // Instance fields
-    private Sprite[][] sprites;
-    private SpriteSelectionScrollPane selectionPane;
-	private boolean isMousePressed;
+	// Instance fields
+	private Sprite[][] sprites;
+	private SpriteSelectionScrollPane selectionPane;
+	private boolean mousePressed;
 
-    /**
-     * Constructor that initializes the editor panel with a grid of sprites.
-     * 
-     * @param ef      the editor frame
-     * @param selPane the sprite selection scroll pane
-     */
-    public EditorPanel(EditorFrame ef, SpriteSelectionScrollPane selPane) {
-        this.selectionPane = selPane;
-        this.sprites = new Sprite[ROWS][COLS];
-        this.isMousePressed = false;
+	/**
+	 * Constructor that initializes the editor panel with a grid of sprites.
+	 * 
+	 * @param ef      the editor frame
+	 * @param selPane the sprite selection scroll pane
+	 */
+	public EditorPanel(EditorFrame ef, SpriteSelectionScrollPane selPane) {
+		this.selectionPane = selPane;
+		this.sprites = new Sprite[ROWS][COLS];
+		this.mousePressed = false;
 
-        setSize();
-        setBackground(Color.BLACK);
-        setLayout(new GridLayout(ROWS, COLS));
-        
-        initializeGrid();
-    }
+		setSize();
+		setBackground(Color.BLACK);
+		setLayout(new GridLayout(ROWS, COLS));
 
-    /**
-     * Initializes the grid with sprites and sets up mouse listeners for interactions.
-     */
-    private void initializeGrid() {
-        for (int y = 0; y < ROWS; y++) {
-            for (int x = 0; x < COLS; x++) {
-                final int xCopy = x;
-                final int yCopy = y;
-                Sprite sprite = new Sprite(x, y, SQUARE_SIZE);
-                sprite.addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mousePressed(MouseEvent e) {
-                        isMousePressed = true;
-                        updateSprite(sprite, xCopy, yCopy);
-                    }
-
-                    @Override
-                    public void mouseReleased(MouseEvent e) {
-                        isMousePressed = false;
-                    }
-                });
-                sprite.addMouseMotionListener(new MouseAdapter() {
-                    @Override
-                    public void mouseDragged(MouseEvent e) {
-                        if (isMousePressed) {
-                            int x = (e.getXOnScreen() - getLocationOnScreen().x) / SQUARE_SIZE;
-                            int y = (e.getYOnScreen() - getLocationOnScreen().y) / SQUARE_SIZE;
-                            try {
-                                updateSprite(sprites[y][x], x, y);
-                            } catch (Exception ignored) {
-                            }
-                        }
-                    }
-                });
-                sprites[y][x] = sprite;
-                add(sprite);
-            }
-        }
-    }
-
-    /**
-     * Custom painting of the panel, renders each sprite in the grid.
-     * 
-     * @param g the Graphics object used for painting
-     */
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        for (Sprite[] row : sprites)
-            for (Sprite s : row)
-                s.render(g);
-    }
-
-    /**
-     * Sets the size of the panel based on the predefined dimensions.
-     */
-    private void setSize() {
-        Dimension size = new Dimension(PANEL_WIDTH, PANEL_HEIGHT);
-        setPreferredSize(size);
-    }
-
-    /**
-     * Loads a level into the editor panel, updating sprites based on the given level data.
-     * 
-     * @param levelData a 2D array representing the level structure
-     */
-    public void loadLevel(char[][] levelData) {
-        for (int y = 0; y < levelData.length; y++) {
-            for (int x = 0; x < levelData[y].length; x++) {
-                if (levelData[y][x] != ' ')
-                    sprites[y][x].updateSpriteImg(AnimationAndImagesLoader.getImage(levelData[y][x]));
-            }
-        }
-        repaint(); // Repaint the panel to reflect the changes
-    }
-
-    /**
-     * Resets the editor panel by clearing all sprites.
-     */
-    public void reset() {
-        for (Sprite[] row : sprites)
-            for (Sprite sprite : row)
-                sprite.updateSpriteImg(null);
-        repaint(); // Repaint the panel to reflect the changes
-    }
-
-    /**
-     * Retrieves the 2D array of sprites representing the grid.
-     * 
-     * @return the 2D array of sprites
-     */
-    public Sprite[][] getSprites() {
-        return sprites;
-    }
-    
-    public SpriteSelectionScrollPane getSelectionPane() {
-		return selectionPane;
+		initializeGrid();
 	}
 
-    /**
-     * Updates the sprite at a specific position with the selected sprite image.
-     * 
-     * @param sprite the sprite to be updated
-     * @param x      the x-coordinate of the sprite in the grid
-     * @param y      the y-coordinate of the sprite in the grid
-     */
-    private void updateSprite(Sprite sprite, int x, int y) {
-        SelectionButton button = selectionPane.getCurrentButton();
-        if (button.getCode() != ' ')
-            sprite.updateSpriteImg(button.getImg());
-        else
-            sprite.updateSpriteImg(null);
-        LevelManager.setTile(y, x, button.getCode());
-        repaint();
-    }
+	/**
+	 * Retrieves the 2D array of sprites representing the grid.
+	 * 
+	 * @return the 2D array of sprites
+	 */
+	public Sprite[][] getSprites() {
+		return sprites;
+	}
+
+	/**
+	 * Retrieves the SpriteSelectionPane
+	 * 
+	 * @return
+	 */
+	public SpriteSelectionScrollPane getSelectionPane() {
+		return selectionPane;
+	}
+	
+	public boolean isMousePressed() {
+		return mousePressed;
+	}
+
+	public void setMousePressed(boolean isMousePressed) {
+		this.mousePressed = isMousePressed;
+	}
+
+	/**
+	 * Initializes the grid with sprites and sets up mouse listeners for
+	 * interactions.
+	 */
+	private void initializeGrid() {
+		for (int y = 0; y < ROWS; y++) {
+			for (int x = 0; x < COLS; x++) {
+				Sprite sprite = new Sprite(x, y, SQUARE_SIZE);
+				sprite.addMouseListener(updateSpriteClick(this, x, y));
+				sprite.addMouseMotionListener(updateSpriteDrag(this, x, y));
+				sprites[y][x] = sprite;
+				add(sprite);
+			}
+		}
+	}
+
+	/**
+	 * Custom painting of the panel, renders each sprite in the grid.
+	 * 
+	 * @param g the Graphics object used for painting
+	 */
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		for (Sprite[] row : sprites)
+			for (Sprite s : row)
+				s.render(g);
+	}
+
+	/**
+	 * Sets the size of the panel based on the predefined dimensions.
+	 */
+	private void setSize() {
+		Dimension size = new Dimension(PANEL_WIDTH, PANEL_HEIGHT);
+		setPreferredSize(size);
+	}
+
+	/**
+	 * Loads a level into the editor panel, updating sprites based on the given
+	 * level data.
+	 * 
+	 * @param levelData a 2D array representing the level structure
+	 */
+	public void loadLevel(char[][] levelData) {
+		for (int y = 0; y < levelData.length; y++) {
+			for (int x = 0; x < levelData[y].length; x++) {
+				if (levelData[y][x] != ' ')
+					sprites[y][x].updateSpriteImg(AnimationAndImagesLoader.getImage(levelData[y][x]));
+			}
+		}
+		repaint(); // Repaint the panel to reflect the changes
+	}
+
+	/**
+	 * Resets the editor panel by clearing all sprites.
+	 */
+	public void reset() {
+		for (Sprite[] row : sprites)
+			for (Sprite sprite : row)
+				sprite.updateSpriteImg(null);
+		repaint(); // Repaint the panel to reflect the changes
+	}
+
+	/**
+	 * Updates the sprite at a specific position with the selected sprite image.
+	 * 
+	 * @param x      the x-coordinate of the sprite in the grid
+	 * @param y      the y-coordinate of the sprite in the grid
+	 */
+	public void updateSprite(int x, int y) {
+		SelectionButton button = selectionPane.getCurrentButton();
+		if (button.getCode() != ' ')
+			sprites[y][x].updateSpriteImg(button.getImg());
+		else
+			sprites[y][x].updateSpriteImg(null);
+		LevelManager.setTile(y, x, button.getCode());
+		repaint();
+	}
 }
